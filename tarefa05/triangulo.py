dfb1afa070ce694a560897a4e1c0d8b2b236d29a
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-04-20 22:59:27 -0300

    correção condição de existência e uso direto da lista

--- tarefa05/triangulo.py
+++ tarefa05/triangulo.py
@@ -1,22 +1,25 @@
 # lê uma string com três partes
-a, b, c = (input())
-lista = [a, b, c]
-lista.sort()
-x, y, z = str(lista).replace(',',' ').replace('\'',' ').strip("[]").split()
+a, b, c = (input()).split()
 
 # converte strings em números
-x = float(x)
-y = float(y)
-z = float(z)
+a = float(a)
+b = float(b)
+c = float(c)
 
+lista = [a, b, c]
+lista.sort()
 
 # termine esse programa aqui...
 
-if (x ** 2)+(y ** 2) == (z ** 2):
-	print('retângulo')
-elif (x ** 2)+(y ** 2) > (z ** 2) and x+y>z:
-	print('acutângulo')
-elif (x ** 2)+(y ** 2) < (z ** 2) and x+y>z:
- 	print('obstusângulo')
+if (lista[0]+lista[1]) > lista[2]:
+
+	if ((lista[0] ** 2) + (lista[1] ** 2)) == (lista[2] ** 2):
+		print('retângulo')
+
+	elif ((lista[0] ** 2) + (lista[1] ** 2)) > (lista[2] ** 2): 
+		print('acutângulo')
+
+	elif ((lista[0] ** 2) + (lista[1] ** 2)) < (lista[2] ** 2): 
+	 	print('obtusângulo')
 else:
 	print('não forma triângulo')
