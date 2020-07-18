7d24bdaeb634932212f3af53162a28288d19ea4d
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-07-17 23:44:28 -0300

    encontrar, criar (novo numero de evento, por keys nao len), remover e listar

--- tarefa12/argumentos.py
+++ tarefa12/argumentos.py
@@ -29,8 +29,8 @@ import argparse
 #usar default None ou ""? 
 parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--agenda", help="nome de sua agenda.", type=string)
-    parser.add_argument("--evento", help="identificador do evento.", default= None, type=int)
-    parser.add_argument("acao", help="ação sobre a agenda.", type=string)
+    parser.add_argument("--evento", help="identificador do evento.", default= None, type=int)   #evento é inteiro, será a chave do dict agenda
+    parser.add_argument("acao", help="ação sobre a agenda.", type=string)   #não tem --, é digitada diretamente no terminal 
     parser.add_argument("--nome", help="nome do evento.", default= None, type=string)
     parser.add_argument("--descricao", help="descrição do evento.", default= None, type=string)
     parser.add_argument("--data", help="data do evento xx/xx/xxxx.", default= None, type=string)
@@ -52,51 +52,87 @@ def inicializar_agenda(nome_arquivo):
     return 
 
 def escrever_arquivo_agenda(nome_arquivo, agenda):
-    with open(nome_arquivo) as arquivo:
+    with open('nome_arquivo', 'w') as arquivo:
         arquivo.write(str(agenda))
     
-def encontrar_evento(agenda, evento):
+def encontrar_evento(agenda, identificador):
     """Recebe o número do evento e devolve o dict do evento"""
-    for numero in agenda:
-        if numero == evento:
-            return agenda[numero] 
+    for evento in agenda:
+        if evento == identificador:     #se chave == numero do evento
+            return agenda[evento]       #devolve o valor da chave do evento
     return None
 
 def criar_evento(agenda, nome, descricao, data, hora):
     """ Cria um novo evento e adiciona ele na agenda"""
+    #cria dict do evento
     evento = {
         "nome": nome, 
         "descricao": descricao, 
         "data": data, 
         "hora": hora
     }
-    numero = len(agenda) + 1
-    agenda[numero] = evento
-    escrever_arquivo_agenda(agenda)
+
+    #como a chave dos eventos não muda quando deletamos um evento, queremos add o novo evento
+    #no numero mais alto possível que ainda não existe no dict, para não apagar outro evento
+    numero_existentes = agenda.keys()   
+    numero_novo = max(numero_existentes) + 1
+    
+    agenda[numero_novo] = evento    #add dict do evento no dict de eventos
     return agenda
 
-def alterar_evento(agenda, evento):
+def alterar_evento(agenda, identificador):
     """Procura evento, altera dict dentro do dict"""
+
     pass
 
-def remover_evento(agenda, evento):
-    pass
+def remover_evento(agenda, identificador):
+    """Remove um evento da agenda pelo numero dele; o numero dos demais 
+    eventos permanece o mesmo"""
+    agenda.pop(identificador)  #evento é o inteiro chave de agenda 
+    return agenda 
 
 def listar_eventos(agenda, data):
-    pass
+    """ Encontra e imprime na tela todos os eventos da agenda na data inserida pelo usuário """
+
+    eventos = []
+    numero_eventos = []
+    identificadores = agenda.keys   #precisamos das chaves para acessar os eventos e para saber o numero deles na hora de imprimir
+
+    for identificador in identificadores:
+        if agenda[identificador]['data'] == data:
+            eventos.append(dicio[identificador])    #lista recebe dicts dos eventos 
+            numero_eventos.append(identificador)    #lista recebe numero do evento
+
+
+    if len(eventos) == 0:   #se len é 0, não há eventos
+        print(f"Não existem eventos para o dia {data}!")
+    else:
+        print(f"Eventos do dia {data}\n")
+        print("-----------------------------------------------\n")
+        for i in range(len(eventos):
+            print(f"Evento {numero_eventos[i]} - {eventos[i]['nome']}")
+            print(f"Descrição: {eventos[i]['descricao']}")
+            print(f"Data: {eventos[i]['data']}")
+            print(f"Hora: {eventos[i]['hora']}")
+            print("-----------------------------------------------")
 
 
 def main():
     agenda = ler_agenda(args.a)
     if args.acao == "inicializar":
-        inicializar_agenda(agenda)
+        inicializar_agenda(args.a)
     elif args.acao == "criar":
         criar_evento(agenda, args.nome, args.descricao, args.data, args.hora):
+        escrever_arquivo_agenda(agenda)    
     elif args.acao == "alterar":
-        alterar_evento(agenda, args.evento):
+        alterar_evento(agenda, args.evento)
+        escrever_arquivo_agenda(agenda)
     elif args.acao = "remover":
         remover_evento(agenda, args.evento)
+        escrever_arquivo_agenda(agenda)
     elif args.acao == "listar":
         listar_eventos(agenda, args.data)
+    else:
+        print(f"Operação {args.acao} é inválida")
     
 main()
