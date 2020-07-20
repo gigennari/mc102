54e94b509e793f6271a8edae2f703aaa806a7e5b
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-07-20 17:35:51 -0300

    collatz.py -- passo a passo dos commits na pasta MC 102 Exemplos de aula

---
+++ tarefa14/fibonacci3.py
@@ -0,0 +1,37 @@
+"""
+Escreve um programa que calcule um Fibonacci modificado
+
+ORIGINAL: 
+0,1,1,2,3,5,8,13,21,34,55...
+
+fib(0) =0
+fib(1) = 1
+fib(2) = 1
+fib(3) = 2
+fib(4) = 3
+Sempre soma os dois últimos números para obter o próximo
+
+MODIFICADO: 
+f(n) = n                            para 0 =< n =< 2
+f(n) = f(n-1) + f(n-2) + f(n-3)     para n > 2
+
+Entrada: um inteiro não negativo
+31
+Saída: o n-ésimo número da sequencia de fibonacci modificado
+83047505
+"""
+
+def fib(n):
+    """ Calcula o fibonacci modificado de um inteiro n"""
+    if 0 <= n <=2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2) + fib(n-3)
+
+
+def main():
+    n = int(input())
+
+    print(fib(n))
+
+main()
