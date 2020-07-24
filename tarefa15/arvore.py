587161beb459ecfd0fcb9e2904d88ad6569e75b7
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-07-24 18:55:45 -0300

    tarefa 15 ok - tudo dentro de uma única função evita problema de acesso aos exs fixação do teste 0

--- tarefa15/arvore.py
+++ tarefa15/arvore.py
@@ -12,65 +12,47 @@ mesmo subdiretório e se o link não foi acessado antes.
 Dúvida 1: como fazer a "quebra" da árvore quando muda a tag?
 Dúvida 2: Não está acessando os links dentro da página fixacao do teste 1
 
-and lista_todas[i] not in lista_printadas
+Solução: fazer tudo na mesma função 
 
 """
 
 import re 
 from modulo import*
 
-def validar_urls(urlbase, urlatual, lista_printadas):
-    """ Cria padrão regex, encontra todas as urls de uma página e resolve e valida cada uma"""
-    htmlatual = obter_html(urlbase) #função do módulo, devolve str
+def criar_arvore(url_inicial, url_atual, url_base, lista_printadas, nivel):
+    """ Printa a url de uma página inicial e toda a hierarquia das páginas que podem ser acessadas a partir da página inicial
+    recursivamente. O recuo da impressão é controlado pelo nível da hierarquia.""" 
 
-    #href/regex
+    #html do url atual passado como parâmetro
+    htmlatual = obter_html(url_atual) #função do módulo, devolve str
+
+    #href/regex -- > identifica links com o padrão html
     href = [""] 
-    padrao = r'"(.*?)"'    #padrão do regex; nongreedy
+    padrao = r'href="(.*?)"'    #padrão do regex; nongreedy
     
-    #re.findall()
-    lista_todas = re.findall(padrao, htmlatual)      
+    #re.findall() --> devolve uma lista com todos os links presentes em uma página inicial, que satisfazem o padrão regex
+    lista_todos = re.findall(padrao, htmlatual)
 
-    #verficar se todas a urls são válidas --> lista_validas
-    lista_validas = [] 
-    for i in range(len(lista_todas)):
-        lista_todas[i] = resolver_url(lista_todas[i], urlbase)
-        if eh_url_valida(lista_todas[i], urlbase):
-            lista_validas.append(lista_todas[i])   #add à lista de urls válidas   
-    return lista_validas
-
-def impressao_recursiva(urlbase, lista_validas, lista_printadas, lista_ramificada, contador):
-    """ Printa as urls que ainda não foram printadas. Quando toda a lista de urls válidas já foi percorrida, chama criar árvore. """
-
-    for i, j in enumerate(lista_validas):
-        '''
-        if i == 0 and j not in lista_printadas:
-            lista_ramificada.append(j)
-            lista_printadas.append(j)
-            novos_validos = validar_urls(urlbase, j, lista_printadas)
-            if len(novos_validos) != 0:
-                impressao_recursiva(urlbase, novos_validos, lista_printadas, lista_ramificada, contador + 2) 
-        '''    
-
-        if j not in lista_printadas:
-            lista_ramificada.append(2*contador*' ' + j)
-            lista_printadas.append(j)
-            novos_validos = validar_urls(urlbase, j, lista_printadas)
-            if len(novos_validos) != 0:
-                impressao_recursiva(urlbase, novos_validos, lista_printadas, lista_ramificada, contador + 1) 
-                
-    return lista_ramificada 
+    #resolve e valida as urls da lista_todos; caso uma url já tenha sido printada, ela não será printada novamente pois está em lista_printadas
+    for url in lista_todos:
+        url = resolver_url(url, url_base)
+        if eh_url_valida(url, url_inicial) and url not in lista_printadas:
+            lista_printadas.append(url)
+            print(f"{nivel*'  '}{url}")
+            criar_arvore(url_inicial, url, url, lista_printadas, nivel+1)   #quando há links dentro um link, o recuo aumenta a partir do nível atual; 
+                                                                            #ao esgotar essa lista, continua a anterior do recuo 'nivel' em que parou
 
 def main():
-    urlinicial = input()
+    #recebe url da página inicial de um site 
+    url_inicial = input()   
 
-    lista_printadas = []
-    lista_validas = validar_urls(urlinicial, urlinicial, lista_printadas)
-    urlbase = urlinicial
-       #acumula todas as url já printadas na tela para evitar repetição
-    lista_ramificada = []
-    arvore = impressao_recursiva(urlbase, lista_validas, lista_printadas, lista_ramificada, 0)
+    #para a primeira recursão, o url_base e o url_atual são iguais ao input
+    url_base = url_inicial  
+    url_atual = url_inicial
 
-    for i in arvore:
-        print(i)
+    lista_printadas = []   #acumula todas as url já printadas na tela para evitar repetição
+    nivel = 0   #a hierarquia começa no nivel 0
+
+    criar_arvore(url_inicial, url_atual, url_base, lista_printadas, nivel)
 
 main()
