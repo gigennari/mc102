623890daf1e3ad0ee7280b909a2447f7eb420b03
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-07-27 10:49:54 -0300

    stubs jobs.py

---
+++ tarefa13/jobs.py
@@ -0,0 +1,40 @@
+"""
+
+ENTRADA:
+numero de tarefas em linhas pares
+grau de dificuldade de cada tarefa nas linhas ímpares
+3
+5 4 2
+
+
+SAÍDA:
+menor diferença possível entre o grau de dificuldade das tarefas
+1
+
+Ideia:
+Percorrer toda a lista e somar todos os elemetos --> peso total
+Criar uma lista vazia de comparação 
+Criar uma variável acumuladora
+
+Percorrer a lista novamente; a cada elemento, soma ele à variável acumuladora 
+Faz a diferença entre peso total e variável acumuladora
+
+Devolve o menor valor da lista de comparação 
+
+"""
+
+
+def comparacao(lista):
+    """ Compara os pesos de n atividades e devolve a menor diferença possível"""
+
+def ler_entrada():
+    """Lê entrada, sendo uma linha com 1 número n e a seguinte com n números"""
+
+
+def main():
+    lista = ler_entrada()
+
+    comparacao()
+
+
+main()
