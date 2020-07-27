b1ee403420e10a94b68aabf843742aebb74b75eb
Giovanna Gennari Jungklaus <g198010@dac.unicamp.br>
2020-07-27 11:30:53 -0300

    jobs.py

--- tarefa13/jobs.py
+++ tarefa13/jobs.py
@@ -17,24 +17,56 @@ Criar uma lista vazia de comparação
 Criar uma variável acumuladora
 
 Percorrer a lista novamente; a cada elemento, soma ele à variável acumuladora 
-Faz a diferença entre peso total e variável acumuladora
+Faz a diferença entre peso total menos o elemento atual
+e variável acumuladora
 
 Devolve o menor valor da lista de comparação 
 
 """
 
 
-def comparacao(lista):
+def comparacao(n, pesos):
     """ Compara os pesos de n atividades e devolve a menor diferença possível"""
+    peso_total = 0
+    for i in range(n):
+        peso_total += int(pesos[i])
+
+    lista_comparativa = []
+    acumuladora = 0
+
+    for i in range(n):  #na última iteração será 0 
+        acumuladora += int(pesos[i])
+        peso_total -= int(pesos[i])
+        diferenca = abs(peso_total - acumuladora)
+        lista_comparativa.append(diferenca)
+
+    menor = lista_comparativa[0]
+    for diferenca in lista_comparativa:
+        if diferenca < menor:
+            menor = diferenca
+
+    return menor
+    
 
 def ler_entrada():
     """Lê entrada, sendo uma linha com 1 número n e a seguinte com n números"""
+    lista = []
+    while True:
+        try:
+            n = int(input())
+            lista.append(n)
+            pesos = input().split()
+            lista.append(pesos)
+        except EOFError:
+            return lista
 
 
 def main():
     lista = ler_entrada()
 
-    comparacao()
+    for i in range(0, len(lista), 2):   #como as linhas vem aos pares, pular de duas em duas 
+        diferenca = comparacao(lista[i], lista[i+1])
+        print(diferenca)
 
 
 main()
